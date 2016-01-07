8db5b0f6bbdf006beb8f4b61d226e84d030a0547
TopGunViper <1449978622@qq.com>
2016-01-07 21:42:15 +0800

    Divide and Conquer,Majority Element...can not sovle it!!!-_-

---
+++ src/edu/ouc/_169_Majority_Element_E.java
@@ -0,0 +1,17 @@
+package edu.ouc;
+
+/**
+ * Given an array of size n, find the majority element. The majority element is the element 
+ * that appears more than n/2 times.
+
+You may assume that the array is non-empty and the majority element always exist 
+in the array.
+ * @author Administrator
+ *
+ */
+public class _169_Majority_Element_E {
+    public int majorityElement(int[] nums) {
+    	
+        return 0;
+    }
+}
