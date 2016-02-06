c4c0146349399532eb4b3bcbc342f88ac939da55
TopGunViper <1449978622@qq.com>
2016-02-06 23:20:06 +0800

    Count of Range Sum

--- src/edu/ouc/_327_Count_of_Range_Sum_H.java
+++ src/edu/ouc/_327_Count_of_Range_Sum_H.java
@@ -1,5 +1,9 @@
 package edu.ouc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  *  Given an integer array nums, return the number of range sums that lie in 
  *  [lower, upper] inclusive.
@@ -18,7 +22,12 @@ The three ranges are : [0, 0], [2, 2], [0, 2] and their respective sums are: -2,
  * 
  */
 public class _327_Count_of_Range_Sum_H {
+	private int lower;
+	private int upper;
     public int countRangeSum(int[] nums, int lower, int upper) {
+    	this.lower = lower;
+    	this.upper = upper;
+    	
         for(int i = 1; i < nums.length; i++){
         	nums[i] = nums[i] + nums[i-1];
         }
@@ -29,4 +38,47 @@ public class _327_Count_of_Range_Sum_H {
         }
         return nums[upper] - nums[lower];
     }
+	public  List<Integer> countSmaller(int[] nums) {
+    	int len = nums.length;
+    	List<Integer> counts = new ArrayList<Integer>();
+    	mergeSort(nums,0,len-1);
+    	return counts;
+	}
+	public  void mergeSort(int nums[],int low,int high){
+		int mid = (low+high)/2;
+		if(low < high){
+			// 左半边
+			mergeSort(nums,low,mid);
+			//右半边
+			mergeSort(nums,mid+1,high);
+			//左右两边有序数组合并
+			merge(nums,low,mid,high);
+		}
+	}
+	
+	public  void merge(int nums[],int low,int mid,int high){
+		int [] tmp = new int[high-low+1];
+		int i = low,j = mid+1,k = 0;
+		int p = mid+1,q = mid+1;
+		while(i <= mid && j <= high){
+			if(nums[i] < nums[j]){
+				tmp[k] = nums[i];
+				k++;i++;
+		        while (p < high && nums[p] - nums[i] < lower) p++;
+		        while (q < high && nums[j] - nums[i] <= upper) q++;
+			}else {
+				tmp[k++] = nums[j++];
+			}
+		}
+		while(i <= mid){
+			tmp[k++] = nums[i++];
+		}
+		while(j <= high){
+			tmp[k++] = nums[j++];
+		}
+		//新的有序数组覆盖旧数组
+		for(p = 0; p < tmp.length; p++){
+			nums[low+p] = tmp[p];
+		}
+	}
 }
