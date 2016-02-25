80651a09157da48328c377c34aeb62ed442e93b3
TopGunViper <1449978622@qq.com>
2016-02-25 23:23:07 +0800

    Greedy! Remove Duplicate letters

---
+++ src/edu/ouc/_316_Remove_Duplicate_Letters_M.java
@@ -0,0 +1,24 @@
+package edu.ouc;
+
+/**
+ *  Given a string which contains only lowercase letters, 
+ *  remove duplicate letters so that every letter appear once and only once.
+You must make sure your result is the smallest in lexicographical order among 
+all possible results.
+
+Example:
+
+Given "bcabc"
+Return "abc"
+
+Given "cbacdcbc"
+Return "acdb" 
+
+ * @author wqx
+ *
+ */
+public class _316_Remove_Duplicate_Letters_M {
+	public String removeDuplicateLetters(String s) {
+
+	}
+}
