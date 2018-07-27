a60bd76760c280c7d61bcff2640d953d5737894e
wuqingxin <wuqingxin@baidu.com>
2018-07-27 09:53:37 +0800

    update readme

--- src/main/java/com/baidu/basic/Box.java
+++ src/main/java/com/baidu/basic/Box.java
@@ -1,18 +1,10 @@
 package com.baidu.basic;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
-
 public class Box<T> {
     private final T t;
 
     public Box(T t) { this.t = t; }
 
     public T get() { return t; }
-
-    public static void main(String[] args) {
-        IntStream
-    }
+    
 }
